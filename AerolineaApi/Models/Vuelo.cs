73574590c034dde1d71307ad91842031faac7bc2
191G0235@rcarbonifera.tecnm.mx <191G0235@rcarbonifera.tecnm.mx>
2022-12-11 23:45:37 -0600

    Agregar archivos de proyecto.

---
+++ AerolineaApi/Models/Vuelo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AerolineaApi.Models
+{
+    public partial class Vuelo
+    {
+        public int Id { get; set; }
+        public string Destino { get; set; } = null!;
+        public string Aerolinea { get; set; } = null!;
+        public DateTime Fecha { get; set; }
+        public int Puerta { get; set; }
+        public int IdObservacion { get; set; }
+
+        public virtual Observacion IdObservacionNavigation { get; set; } = null!;
+    }
+}
---
+++ AerolineaApi/Models/sistem21_aerolineaContext.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AerolineaApi.Models
+{
+    public partial class sistem21_aerolineaContext : DbContext
+    {
+        public sistem21_aerolineaContext()
+        {
+        }
+
+        public sistem21_aerolineaContext(DbContextOptions<sistem21_aerolineaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Observacion> Observacion { get; set; } = null!;
+        public virtual DbSet<Vuelo> Vuelo { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8_general_ci")
+                .HasCharSet("utf8");
+
+            modelBuilder.Entity<Observacion>(entity =>
+            {
+                entity.ToTable("observacion");
+
+                entity.Property(e => e.Id)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Observacion1)
+                    .HasMaxLength(45)
+                    .HasColumnName("observacion");
+            });
+
+            modelBuilder.Entity<Vuelo>(entity =>
+            {
+                entity.ToTable("vuelo");
+
+                entity.HasIndex(e => e.IdObservacion, "fkVueloObservacion_idx");
+
+                entity.Property(e => e.Id)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Aerolinea)
+                    .HasMaxLength(45)
+                    .HasColumnName("aerolinea");
+
+                entity.Property(e => e.Destino)
+                    .HasMaxLength(45)
+                    .HasColumnName("destino");
+
+                entity.Property(e => e.Fecha)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fecha");
+
+                entity.Property(e => e.IdObservacion)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("id_observacion");
+
+                entity.Property(e => e.Puerta)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("puerta");
+
+                entity.HasOne(d => d.IdObservacionNavigation)
+                    .WithMany(p => p.Vuelo)
+                    .HasForeignKey(d => d.IdObservacion)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fkVueloObservacion");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
