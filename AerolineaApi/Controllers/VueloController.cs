f95d5b3c872d2d831c896e4a24711d56aee5f98d
191G0235@rcarbonifera.tecnm.mx <191G0235@rcarbonifera.tecnm.mx>
2022-12-13 00:36:04 -0600

    cambios

--- AerolineaApi/Controllers/VueloController.cs
+++ AerolineaApi/Controllers/VueloController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AerolineaApi.DTOs;
+using AerolineaApi.Models;
+using AerolineaApi.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AerolineaApi.Controllers
@@ -7,6 +10,26 @@ namespace AerolineaApi.Controllers
     [ApiController]
     public class VueloController : ControllerBase
     {
-       
+        private readonly sistem21_aerolineaContext context;
+        Repository<Vuelo> repository;
+
+        public VueloController(sistem21_aerolineaContext context)
+        {
+            this.context = context;
+            repository = new(context);
+        }
+
+        [HttpPost]
+        public IActionResult Post(VueloDTO vuelo)
+        {
+        //     public int Id { get; set; }
+        //public string Destino { get; set; } = null!;
+        //public string Aerolinea { get; set; } = null!;
+        //public DateTime Fecha { get; set; }
+        //public int Puerta { get; set; }
+        //public int IdObservacion { get; set; }
+
+           if()
+        }
     }
 }
---
+++ AerolineaApi/DTOs/VueloDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace AerolineaApi.DTOs
+{
+    public class VueloDTO
+    {
+        public string Destino { get; set; } = null!;
+        public string Aerolinea { get; set; } = null!;
+        public DateTime Fecha { get; set; }
+        public int Puerta { get; set; }
+        public int IdObservacion { get; set; }
+    }
+}
