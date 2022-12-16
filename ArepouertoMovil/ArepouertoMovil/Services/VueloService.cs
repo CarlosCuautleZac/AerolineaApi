fd9e283d30d122bed651744c1456551de1483eaa
191G0235@rcarbonifera.tecnm.mx <191G0235@rcarbonifera.tecnm.mx>
2022-12-15 22:24:21 -0600

    cambios

---
+++ ArepouertoMovil/ArepouertoMovil/Services/VueloService.cs
@@ -0,0 +1,40 @@
+﻿using ArepouertoMovil.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArepouertoMovil.Services
+{
+    public class VueloService
+    {
+        HttpClient client = new HttpClient()
+        {
+            BaseAddress = new Uri("https://aerolinea.sistemas19.com/api/vuelo")
+        };
+
+        public async Task<List<Vuelo>> Get()
+        {
+            List<Vuelo> vuelos = new List<Vuelo>();
+            var response = await client.GetAsync("");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                vuelos = JsonConvert.DeserializeObject<List<Vuelo>>(json);
+            }
+
+            if (vuelos == null)
+                return new List<Vuelo>();
+            else
+                return vuelos;
+
+        }
+
+        public async Task Put(Vuelo vuelo)
+        {
+            
+        }
+    }
+}
